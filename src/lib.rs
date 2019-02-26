ba1464de7ea5d259c324abc8a4959ddcdcbc6000
Sumeet Agarwal <sumeet.a@gmail.com>
2019-02-26 19:57:51 +0900

    fix to work on nightly

--- src/lib.rs
+++ src/lib.rs
@@ -38,23 +38,25 @@
 
 use std::ops::{Generator, GeneratorState};
 use std::iter::Iterator;
+use std::marker::Unpin;
+use std::pin::Pin;
 
 /// a iterator that holds an internal generator representing
 /// the iteration state
 #[derive(Copy, Clone, Debug)]
 pub struct GenIter<T>(pub T)
 where
-    T: Generator<Return = ()>;
+    T: Generator<Return = ()> + Unpin;
 
 impl<T> Iterator for GenIter<T>
 where
-    T: Generator<Return = ()>,
+    T: Generator<Return = ()> + Unpin,
 {
     type Item = T::Yield;
 
     #[inline]
     fn next(&mut self) -> Option<Self::Item> {
-        match unsafe { self.0.resume() } {
+        match Pin::new(&mut self.0).resume() {
             GeneratorState::Yielded(n) => Some(n),
             GeneratorState::Complete(()) => None,
         }
@@ -63,7 +65,7 @@ where
 
 impl<G> From<G> for GenIter<G>
 where
-    G: Generator<Return = ()>,
+    G: Generator<Return = ()> + Unpin,
 {
     #[inline]
     fn from(gen: G) -> Self {
